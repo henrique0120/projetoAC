221a86fa58ff6b4416be425b4a31eb279ce6474a
henrique.souza <henrique.sl959@hotmail.com>
2026-05-15 16:01:37 -0300

    15/05/26 postgresql e docker e tests

--- src/test/java/io/github/henrique0120/projetonsei/AGRRepositoryTests.java
+++ src/test/java/io/github/henrique0120/projetonsei/AGRRepositoryTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
@@ -15,20 +17,38 @@ public class AGRRepositoryTests {
     AGRRepository repository;
 
     @Test
-    public void salvarTest(){
+    public void registerTest(){
         AGR agr = new AGR();
-        agr.setCpf("12345678910");
-        agr.setName("henrique");
-        agr.setEmail("henrique@email.com");
+        agr.setCpf("92748578320");
+        agr.setName("joao");
+        agr.setEmail("joao@email.com");
 
         var attAgr = repository.save(agr);
         System.out.println(attAgr);
 
     }
 
-//    @Test
-//    public void atualizarTest(){
-//        var id = UUID.fromString("1");
-//    }
+    @Test
+    public void updateTest(){
+        var id = UUID.fromString("6f50c74d-8f5f-4ef9-9c2d-7e6da0e0b0a8");
+
+        Optional<AGR> possibleAGR = repository.findById(id);
+        AGR agr = new AGR();
+
+        if (possibleAGR.isPresent()){
+            AGR autorEncontrado = possibleAGR.get();
+            autorEncontrado.setName("henrique de souza silva");
+            repository.save(autorEncontrado);
+        }
+    }
+
+    @Test
+    public void getAGR(){
+        List<AGR> listagrs = repository.findAll();
+        for (AGR listagr : listagrs) {
+            System.out.println(listagr);
+        }
+
+    }
 
 }
