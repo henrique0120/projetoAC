e7b9341448977d1412aeccad4b70a1e659b5882c
henrique.souza <henrique.sl959@hotmail.com>
2026-06-09 16:04:17 -0300

    09/06/26 Usando MapStruct no método Post

--- src/main/java/io/github/henrique0120/projetonsei/common/GlobalExceptionHandler.java
+++ src/main/java/io/github/henrique0120/projetonsei/common/GlobalExceptionHandler.java
@@ -11,6 +11,4 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.CONFLICT)
-    }
 }
